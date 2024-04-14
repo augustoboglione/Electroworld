0fd93d1477e47b68e080184235727f8392d68efa
Augusto Boglione Carrea <aguboglione@gmail.com>
2024-04-14 13:28:18 -0300

    cart widget

--- js/cart.js
+++ js/cart.js
@@ -1,38 +1,87 @@
-const names = ["processor", "motherboard", "ssd", "ram", "card"]
-const prices = {"processor": 3, "motherboard": 5, "ssd": 10, "ram": 8, "card": 11}
-const products = names.map(el => new Product(el))
+const ids = ["processor", "motherboard", "ssd", "ram", "graphics"]
+const text = {"processor": "Procesador", "motherboard": "Motherboard", "ssd": "Tarjeta SSD", "ram": "Tarjeta RAM", "graphics": "Tarjeta gráfica"}
+const prices = {"processor": 3, "motherboard": 5, "ssd": 10, "ram": 8, "graphics": 11}
 
-const cartWidget = document.getElementById("cart-widget")
+const cart = document.querySelector("#cart")
+const cartList = document.querySelector(".cart-list")
+const total = document.querySelector(".total")
+
+const getLocalStorage = () => {
+    if (localStorage.getItem(ids[0]) != null) ids.forEach((el, i) => products[i].qty = parseInt(localStorage.getItem(el)))
+}
 
 class Product {
-    constructor(name) {
-        this.name = name
-        this.price = prices[name]
+    constructor(id) {
+        this.id = id
+        this.price = prices[id]
         this.qty = 0
-        this.button = document.getElementById(name)
+
+        this.button = document.getElementById(id)
+        if (this.button != null) this.button.onclick = this.addToCart.bind(this)
 
         this.item = document.createElement("div")
         this.item.className = "item"
-        this.item.innerHTML = `<p>${this.name}</p><img class="down"><p class="qty"></p><img class="up"><img class="delete">`
+        this.item.innerHTML = `<p class="name">${text[this.id]}</p><p class="down">-</p>
+        <p class="qty"/><p class="up">+</p><img class="delete" src="../img/bin.png"><p class="price"/>`
+    
+        this.addToCart = this.addToCart.bind(this)
+        this.up = this.up.bind(this)
+        this.down = this.down.bind(this)
+        this.delete = this.delete.bind(this)
+    }
 
-        this.button.onclick = this.addToCart
+    save() {
+        const num = products.reduce((x, y) => x + y.qty, 0)
+
+        this.item.querySelector(".qty").innerText = this.qty
+        this.item.querySelector(".price").innerText = `$${this.qty * this.price}`
+
+        cart.querySelector(".cart-num").innerText = num
+
+        if (num) total.innerText = products.reduce((x, y) => x + y.qty * y.price, 0)
+        else total.innerText = "El carrito está vacío."
+
+        localStorage.setItem(this.id, this.qty)
+    }
+
+    addItem() {
+        if (!cartList.contains(this.item) && this.qty) {
+            cartList.insertBefore(this.item, total)
+
+            this.item.querySelector(".down").onclick = this.down
+            this.item.querySelector(".up").onclick = this.up
+            this.item.querySelector(".delete").onclick = this.delete
+        }
+        this.save()
     }
 
     addToCart() {
-        this.qty++
-        this.item.querySelector(".qty").innerText = this.qty
-        if (!cart.includes(this.entry)) {
-            cartWidget.appendChild(this.entry)
+        this.up()
+        this.addItem()
+    }
 
-            this.item.querySelector("down").onclick = () => this.qty == 1 ? this.delete : this.qty-- 
-            this.item.querySelector("up").onclick = () => this.qty++
-            this.item.querySelector("delete").onclick = this.delete
+    up() {
+        this.qty++
+        this.save()
+    }
+
+    down() {
+        if (this.qty == 1) this.delete()
+        else {
+            this.qty--
+            this.save()
         }
     }
 
     delete() {
         this.qty = 0
-        cartWidget.removeChild(this.entry)
+        this.save()
+        cartList.removeChild(this.item)
     }
 }
 
+const products = ids.map(el => new Product(el))
+
+getLocalStorage()
+
+for (let i of products) i.addItem()
